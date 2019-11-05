d4b74e97c29054334bd742c75a9e8f400a456b6e
Oleg Borschev <bosoleg89@gmail.com>
2019-11-05 17:18:52 +0200

    Updates

---
+++ precache-manifest.4be13b3a87a38d85f18c4001f19d8666.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d784a805fd6274a3391d0ef62b48f007",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "a493e2858f2eecb70227",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.8f8f81c1.chunk.js"
+  },
+  {
+    "revision": "1093f7fc6a225ed4d8fe",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.9cd4ad32.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oBorschev/goit-react-hw-02-bank-account/precache-manifest.7d0810a41017c874def7cd24c73cae82.js"
+  "/goit-react-hw-02-bank-account/precache-manifest.4be13b3a87a38d85f18c4001f19d8666.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oBorschev/goit-react-hw-02-bank-account/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02-bank-account/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
